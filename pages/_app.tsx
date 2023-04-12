bf268de37e1576ae645151a3cfd71e09c900422d
dn0418 <daniel.noble0418@gmail.com>
2023-04-12 19:50:26 +0800

    Initial commit from Create Next App

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "../utils/createEmotionCache";
+import defaultTheme from "../src/themes/defaultTheme";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+// Extend the default AppProps
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  // If there's no emotionCache rendered by the server, use the clientSideEmotionCache
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={defaultTheme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+
+import { Typography } from "@mui/material";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Typography variant="h2" textAlign="center">
+        Hello, World
+      </Typography>
+    </div>
+  );
+};
+
+export default Home;
